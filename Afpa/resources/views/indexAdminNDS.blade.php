d38e498f695d44407f5c0fadd83eb54a4c3192fb
AntoineWtz <awtz.dev@gmail.com>
2023-04-12 13:02:36 +0200

    aw:text

--- Afpa/resources/views/indexAdminNDS.blade.php
+++ Afpa/resources/views/indexAdminNDS.blade.php
@@ -21,28 +21,28 @@
         <div class="bg-white rounded-2xl m-20 text-center">
             <div class="text-center">
                 <a href="{{ route('createNDS') }}">
-                    <button class="bg-green-500 rounded-xl m-4 p-2">Ajouter</button>
+                    <button class="bg-green-600 rounded-xl m-4 p-2">Ajouter button | +</button>
                 </a>
             </div>
             <table class="table-auto mx-auto">
                 <thead>
                     <tr>
-                        <th class="px-4 py-2 text-center">Titre</th>
-                        <th class="px-4 py-2 text-center">Auteur</th>
-                        <th class="px-4 py-2 text-center">Date de création</th>
-                        <th class="px-4 py-2 text-center">Etat</th>
-                        <th class="px-4 py-2 text-center">Visibilité</th>
+                        <th class="px-8 py-4 text-center uppercase">Titre</th>
+                        <th class="px-8 py-4 text-center uppercase">Auteur</th>
+                        <th class="px-8 py-4 text-center uppercase">Date de création</th>
+                        <th class="px-8 py-4 text-center uppercase">Etat</th>
+                        <th class="px-8 py-4 text-center uppercase">Visibilité</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($notes_de_service as $note_de_service)
                     <tr class="border-t-2">
-                        <td class="px-4 py-2 align-middle text-center border-2 border-solid border-black">{{ $note_de_service->titre }}</td>
-                        <td class="px-4 py-2 align-middle text-center border-2 border-solid border-black">{{ $note_de_service->auteur }}</td>
-                        <td class="px-4 py-2 align-middle text-center border-2 border-solid border-black">{{ $note_de_service->date_creation }}</td>
-                        <td class="px-4 py-2 align-middle text-center border-2 border-solid border-black">{{ $note_de_service->visibilite->nom }}</td>
-                        <td class="px-4 py-2 align-middle text-center border-2 border-solid border-black">{{ $note_de_service->etat->nom }}</td>
-                        <td class="px-4 py-2 align-middle flex justify-center m-2">
+                        <td class="px-auto py-auto align-middle text-center border-2 border-solid border-black capitalize">{{ $note_de_service->titre }}</td>
+                        <td class="px-auto py-auto align-middle text-center border-2 border-solid border-black capitalize">{{ $note_de_service->auteur }}</td>
+                        <td class="px-auto py-auto align-middle text-center border-2 border-solid border-black capitalize">{{ $note_de_service->date_creation }}</td>
+                        <td class="px-auto py-auto align-middle text-center border-2 border-solid border-black capitalize">{{ $note_de_service->visibilite->nom }}</td>
+                        <td class="px-auto py-auto align-middle text-center border-2 border-solid border-black capitalize">{{ $note_de_service->etat->nom }}</td>
+                        <td class="px-auto py-auto align-middle flex justify-center m-2">
                             <a href="{{ route('selectNDS', ['slug' => $note_de_service['slug']]) }}">Modifier</a>
                             <form action="{{ route('DeleteNDS', ['slug' => $note_de_service['slug']]) }}" method="post">
                                 @csrf

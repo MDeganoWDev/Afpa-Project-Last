8e709ad340b9e3ec0fd6ce999966c2817c5c1fa8
AntoineWtz <awtz.dev@gmail.com>
2023-04-12 13:43:49 +0200

    aw:OK-IndexNCSAdmin

--- Afpa/resources/views/indexAdminNDS.blade.php
+++ Afpa/resources/views/indexAdminNDS.blade.php
@@ -18,11 +18,12 @@
         @if(session('success'))
         <div>{{ session('success') }}</div>
         @endif
-        <div class="bg-white rounded-2xl m-20 text-center p-8">
-            <a href="{{ route('createNDS') }}" class="text-center">
-                <button class="bg-green-600 rounded-xl m-4 p-2">Ajouter button | +</button>
+        <div class="bg-white rounded-2xl m-20 p-8 shadow-2xl">
+            <a href="{{ route('createNDS') }}" class="text-center flex justify-center items-start">
+                <button class="bg-green-600 rounded-xl m-4 p-2 shadow-2xl text-white">Ajouter button | +</button>
             </a>
-            <table class="table-auto mx-auto">
+
+            <table class="table-auto mx-auto max-w-full">
                 <thead>
                     <tr>
                         <th class="px-8 py-4 text-center uppercase">Titre</th>
@@ -35,17 +36,17 @@
                 <tbody>
                     @foreach ($notes_de_service as $note_de_service)
                     <tr>
-                        <td class="align-middle text-center border-2 border-solid border-black capitalize">{{ $note_de_service->titre }}</td>
-                        <td class="align-middle text-center border-2 border-solid border-black capitalize">{{ $note_de_service->auteur }}</td>
-                        <td class="align-middle text-center border-2 border-solid border-black capitalize">{{ $note_de_service->date_creation }}</td>
-                        <td class="align-middle text-center border-2 border-solid border-black capitalize">{{ $note_de_service->visibilite->nom }}</td>
-                        <td class="align-middle text-center border-2 border-solid border-black capitalize">{{ $note_de_service->etat->nom }}</td>
-                        <td class="align-middle flex text-center">
-                            <a href="{{ route('selectNDS', ['slug' => $note_de_service['slug']]) }}"><button class="bg-blue-600 rounded-xl m-2 p-2">Modifier | #</button></a>
+                        <td class="text-center border-2 border-solid border-black capitalize">{{ $note_de_service->titre }}</td>
+                        <td class="text-center border-2 border-solid border-black capitalize">{{ $note_de_service->auteur }}</td>
+                        <td class="text-center border-2 border-solid border-black capitalize">{{ $note_de_service->date_creation }}</td>
+                        <td class="text-center border-2 border-solid border-black capitalize">{{ $note_de_service->visibilite->nom }}</td>
+                        <td class="text-center border-2 border-solid border-black capitalize">{{ $note_de_service->etat->nom }}</td>
+                        <td class="flex text-center">
+                            <a href="{{ route('selectNDS', ['slug' => $note_de_service['slug']]) }}"><button class="bg-blue-600 rounded-xl m-2 p-2 text-white">Modifier | #</button></a>
                             <form action="{{ route('DeleteNDS', ['slug' => $note_de_service['slug']]) }}" method="post">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="bg-red-600 rounded-xl m-2 p-2">Supprimer | X</button>
+                                <button type="submit" class="bg-red-600 rounded-xl m-2 p-2 text-white">Supprimer | X</button>
                             </form>
                         </td>
                     </tr>

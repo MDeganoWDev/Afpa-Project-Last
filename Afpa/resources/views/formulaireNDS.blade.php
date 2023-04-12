ed8f3039b97db3eca55387093ddfecdb04392012
AntoineWtz <awtz.dev@gmail.com>
2023-04-12 16:11:54 +0200

    aw:AdminFormNDS

--- Afpa/resources/views/formulaireNDS.blade.php
+++ Afpa/resources/views/formulaireNDS.blade.php
@@ -26,21 +26,19 @@
                 </div>
                 <img src="{{ asset('images/afpa.jpg') }}" alt="logo AFPA" width="200" height="200">
             </div>
-
+            @endif
             <div class="bg-white rounded-2xl m-20 p-10 shadow-2xl">
                 <form action="{{ route('saveNDS') }}" method="post" enctype="multipart/form-data" class="flex justify-evenly">
-                    @endif
-
                     @csrf
                     <div class="text-left">
-                        <div class="form-group m-4">
+                        <div class="m-4">
                             <label for="titre">Titre</label>
                             <input type="text" class="form-control @error('titre') is-invalid @enderror" id="titre" name="titre" required value="{{ isset($note_de_service) ? $note_de_service->titre : old('titre') }}">
                             @error('titre')
                             <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
-                        <div class="form-group m-4 p-4">
+                        <div class=" m-4 p-4">
                             <label for="pdf">PDF</label>
                             @if(isset($note_de_service))
                             <p>Le fichier PDF actuel est :</p>
@@ -55,35 +53,33 @@
                         </div>
                     </div>
                     <div class="text-right">
-                        <div class="form-group m-4 p-4">
+                        <div class=" m-4 p-4">
                             <label for="auteur">Auteur</label>
                             <input type="text" class="form-control @error('auteur') is-invalid @enderror" id="auteur" name="auteur" required value="{{ isset($note_de_service) ? $note_de_service->auteur : old('auteur') }}">
                             @error('auteur')
                             <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
-                        <div class="form-group m-4 p-4">
+                        <div class=" m-4 p-4">
                             <label for="date_creation">Date de création</label>
                             <input type="date" class="form-control @error('date_creation') is-invalid @enderror" id="date_creation" name="date_creation" required value="{{ isset($note_de_service) ? $note_de_service->date_creation : old('date_creation') }}">
                             @error('date_creation')
                             <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
-                            <div class="form-group m-4 p-4">
-                                <div class="form-group m-4 p-4">
-                                    <label for="etat">État</label>
-                                    <select class="form-control" id="etat" name="etat">
-                                        @foreach ($etats as $etat)
-                                        <option value="{{ $etat->id }}" @if(isset($note_de_service) && $note_de_service->etat_id == $etat->id) selected @endif>{{ $etat->nom }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <label for="visibilite">Visibilité</label>
-                                <select class="form-control" id="visibilite" name="visibilite">
-                                    @foreach ($visibilites as $visibilite)
-                                    <option value="{{ $visibilite->id }}" @if(isset($note_de_service) && $note_de_service->visibilite_id == $visibilite->id) selected @endif>{{ $visibilite->nom }}</option>
+                            <div class="f m-4 p-4">
+                                <label for="etat">État</label>
+                                <select class="form-control" id="etat" name="etat">
+                                    @foreach ($etats as $etat)
+                                    <option value="{{ $etat->id }}" @if(isset($note_de_service) && $note_de_service->etat_id == $etat->id) selected @endif>{{ $etat->nom }}</option>
                                     @endforeach
                                 </select>
                             </div>
+                            <label for="visibilite">Visibilité</label>
+                            <select class="form-control" id="visibilite" name="visibilite">
+                                @foreach ($visibilites as $visibilite)
+                                <option value="{{ $visibilite->id }}" @if(isset($note_de_service) && $note_de_service->visibilite_id == $visibilite->id) selected @endif>{{ $visibilite->nom }}</option>
+                                @endforeach
+                            </select>
                         </div>
                     </div>
                 </form>
